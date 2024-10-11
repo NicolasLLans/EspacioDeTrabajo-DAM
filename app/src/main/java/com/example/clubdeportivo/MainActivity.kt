cf138eef3851c4b7c2b3f9f41e8e639fbeb6e75f
Alexa-2k <109705749+Alexa-2k@users.noreply.github.com>
2024-10-11 16:18:00 -0300

    Actividad Nuevo empleado 1

--- app/src/main/java/com/example/clubdeportivo/MainActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/MainActivity.kt
@@ -1,16 +1,17 @@
-package com.example.clinicaseprise
+package com.example.clubdeportivo
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.clubdeportivo.R
 
-class dashboard : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
