cbfa755775f34dcc16c1ce569cde1c56e043f336
Oscar Julian Sosa Ortiz <okijulian0791@gmai.com>
2024-10-12 19:09:27 -0300

    - se crea  activity_vencimientos_cuota.xml y se arregla el using de activity_vencimiento_de_cuota1.xml

--- app/src/main/java/com/example/clubdeportivo/vencimientosCuota.kt
+++ app/src/main/java/com/example/clubdeportivo/vencimientosCuota.kt
@@ -5,13 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.clubdeportivo.R
 
-class vencimientoDeCuota1 : AppCompatActivity() {
+class vencimientosCuota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_vencimiento_de_cuota1)
+        setContentView(R.layout.activity_vencimientos_cuota)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
