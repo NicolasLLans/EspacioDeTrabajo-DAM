5001ccb27e3c301d35f624120bb4f5e3326c67b6
Luciano Madroñal <lucianoumc1@gmail.com>
2024-10-13 19:35:11 -0300

    apply styles - partial

--- app/src/main/java/com/example/clubdeportivo/vencimientosCuota.kt
+++ app/src/main/java/com/example/clubdeportivo/vencimientosCuota.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.clinicaseprise.R
 
 class vencimientosCuota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
